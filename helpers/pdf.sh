7ab1f5e8158347df3206c70cda0e4af0f9938b95
elpekenin <monopatinct@gmail.com>
2022-08-27 02:38:48 +0200

    fix pdf script

--- helpers/pdf.sh
+++ helpers/pdf.sh
@@ -1,28 +1,27 @@
 #!/usr/bin/zsh
 
 # Function to compile the LaTeX files
-pdf () {
-  echo 'Cleaning previous compilation files ...'
-  rm /tmp/main*
+echo 'Cleaning previous compilation files ...'
+rm /tmp/main*
   
-  echo 'Compiling ...'
-  cd "${_MY_PROJECT_PATH}latex" && pdflatex -output-directory=/tmp main.tex > /dev/null 2>&1
+echo 'Compiling ...'
+cd ../latex && pdflatex -output-directory=/tmp main.tex > /dev/null 2>&1
   
-  echo 'Adding references...'
-  biber /tmp/main.bcf > /dev/null 2>&1
+echo 'Adding references...'
+biber /tmp/main.bcf > /dev/null 2>&1
    
-  echo 'Recompiling ...'
-  cd "${_MY_PROJECT_PATH}latex" && pdflatex -output-directory=/tmp main.tex > /dev/null 2>&1
+echo 'Recompiling ...'
+pdflatex -output-directory=/tmp main.tex > /dev/null 2>&1
   
-  echo 'Copying file to working directory ...'
-  cp /tmp/main.pdf report.pdf
+echo 'Copying file to working directory ...'
+cp /tmp/main.pdf report.pdf
   
-  if pgrep -x "mupdf" > /dev/null
-  then # just in case, to avoid file not re-rendering upon re-compile
-    echo 'Killing mupdf ...'
-    pkill mupdf
-  fi
+if pgrep -x "mupdf" > /dev/null
+then # just in case, to avoid file not re-rendering upon re-compile
+  echo 'Killing mupdf ...'
+  pkill mupdf
+fi
 
-  echo 'Opening report ...'
-  (mupdf report.pdf > /dev/null 2>&1 &)
-}
+echo 'Opening report ...'
+(mupdf report.pdf > /dev/null 2>&1 &)
+
